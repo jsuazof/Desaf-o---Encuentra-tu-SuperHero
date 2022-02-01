497d070a39b3837751badc7418b6eea405a7fbfd
Jorge <93343250+jsuazof@users.noreply.github.com>
2022-02-01 08:36:20 -0300

    Avance SuperHero
    Avance SuperHero

--- apps.js
+++ apps.js
@@ -1,27 +1,45 @@
 $(() => {
     const btnPosts = $("#btnPosts");
     const resultadoPosts = $("#resultadoPosts");
+    const inputBuscar = $("#inputBuscar");
+    const form = $("#form")
+
+
+    form.on("submit", (e) => {
+        e.preventDefaul();
+    })
+
+    $('img.logo').parent().css('text-align','center');
   
     btnPosts.on("click", () => {
       console.log("me diste click");
       $.ajax({
-        url: "https://jsonplaceholder.typicode.com/posts",
+        url: "https://www.superheroapi.com/api.php/3525635500807579/${inputBuscar.val()}",
         type: "GET",
         dataType: "JSON",
         success(data) {
           console.log(data);
-          data.forEach((item) => {
+          
             resultadoPosts.append(`
-            <article class="col-12 col-md-4 mb-2">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">${item.id} - ${item.title}</h5>
-                        <p class="lead">${item.body}</p>
+                <article class="col-12 col-md-4 mb-2">
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img src="..." class="img-fluid rounded-start" alt="...">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                    <h5 class="card-title">${data.name}</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </article>
+                </article>
+            
             `);
-          });
+          
         },
         error(err) {
           console.log(err);
